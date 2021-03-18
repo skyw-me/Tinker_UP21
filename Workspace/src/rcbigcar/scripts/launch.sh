db2c4f2264e4d6287bc1750d4d3959644adf5d4d
One <imone@tuta.io>
2021-03-18 15:57:17 +0800

    setup script export ip

--- Workspace/src/rcbigcar/scripts/launch.sh
+++ Workspace/src/rcbigcar/scripts/launch.sh
@@ -7,6 +7,10 @@ WORKSPACE_PATH=$(readlink --canonicalize "$SCRIPT_PATH/../../../")
 source /opt/ros/noetic/setup.bash
 source "${WORKSPACE_PATH}/devel/setup.bash"
 
+# Set ROS Master and IP
+export ROS_MASTER_URI=http://localhost:11311
+export ROS_IP=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
+
 # Setup CAN
 "${SCRIPT_PATH}/setup_can.sh"
 
