3b9adba3ae6f53add61d7f3ecde69918d6992188
One <imone@tuta.io>
2021-03-18 15:17:26 +0800

    async spinner and set scheduler priority

--- Workspace/src/rcbigcar/src/robot/robot.cpp
+++ Workspace/src/rcbigcar/src/robot/robot.cpp
@@ -8,6 +8,16 @@ int main(int argc, char **argv)
 
 	ros::NodeHandle nh;
 
+	// We run the ROS loop in a separate thread as external calls, such
+	// as service callbacks loading controllers, can block the (main) control loop
+	ros::AsyncSpinner spinner(1);
+	spinner.start();
+
+	// set process priority
+	struct sched_param params{.sched_priority = 98};
+  	if (sched_setscheduler(0, SCHED_FIFO, &params) == -1)
+    	ROS_ERROR("Set scheduler failed, RUN THIS NODE AS SUPER USER.\n");
+
 	//Create Nodes
 	Chassis chassis;
 	Motion motion;
@@ -24,12 +34,9 @@ int main(int argc, char **argv)
 		//Update Hardware
 		Hardware()->update();
 
-		//Spin
-		ros::spinOnce();
+		//Loop
 		loop_rate.sleep();
 	}
-
-	//Release Nodes
 
 	//Release Hardware
 	ReleaseHardware();
