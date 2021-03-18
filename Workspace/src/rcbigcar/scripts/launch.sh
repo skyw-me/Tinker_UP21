1a0d2a0962152cddb3bc19c7af30a278cc1df49c
One <imone@tuta.io>
2021-03-18 15:49:30 +0800

    update launch script

--- Workspace/src/rcbigcar/scripts/launch.sh
+++ Workspace/src/rcbigcar/scripts/launch.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # Load ROS Environment
+SCRIPT_PATH=$(dirname `realpath "$0"`)
+WORKSPACE_PATH=$(readlink --canonicalize "$SCRIPT_PATH/../../../")
+
 source /opt/ros/noetic/setup.bash
+source "${WORKSPACE_PATH}/devel/setup.bash"
 
 # Setup CAN
-./setup_can.sh
+"${SCRIPT_PATH}/setup_can.sh"
 
 # Launch controller nodes
 roslaunch rcbigcar all.launch
